a9c98d12074fc11e239f2be67b8cba75e37edd3e
Eduarda Lima <eududalim@gmail.com>
2023-03-18 10:53:20 -0300

    feat: created controller with ValueNotifier native

--- lib/app/layers/presenters/controllers/details_controller.dart
+++ lib/app/layers/presenters/controllers/details_controller.dart
@@ -1,5 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:movie_challenge_flutter/app/layers/data/repositories/movies_repository.dart';
+import 'package:movie_challenge_flutter/app/layers/domain/states/list_movies_states_model.dart';
+
 class DetailsController {
-  DetailsController() {
-    //
+  final MoviesRepository _repository;
+  DetailsController(this._repository);
+
+  final movieState = ValueNotifier<MoviesState>(MoviesInitialState());
+
+  loadDetailMovie() {
+    movieState.value = MoviesLoadingState();
+    _repository.getDetailsMovie().then((value) => movieState.value = value);
   }
 }
